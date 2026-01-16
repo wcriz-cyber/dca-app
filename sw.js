cc84d1916bf632f54fde8c64a8e7a0f59a40dc11
wcriz-cyber <wcriz@proton.me>
2026-01-16 18:34:59 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,18 @@
-const CACHE_NAME = 'c5x-dca-v19-cache';
-const ASSETS = [
+const CACHE_NAME = 'c5x-cache-v1';
+const assets = [
   './',
   './index.html',
+  './manifest.json',
   'https://fonts.googleapis.com/css2?family=Orbitron:wght@600;900&family=Roboto:wght@400;700;900&family=Roboto+Mono:wght@700;900&display=swap'
 ];
 
-// Instalación y guardado en caché
-self.addEventListener('install', (e) => {
+self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
   );
 });
 
-// Estrategia: Primero buscar en Red, si falla, usar Caché
-self.addEventListener('fetch', (e) => {
+self.addEventListener('fetch', e => {
   e.respondWith(
     fetch(e.request).catch(() => caches.match(e.request))
   );
